c3f51924565193c8f166ae10c34306b742cf2d5b
22p26c0377 <pawut.j@gmail.com>
2021-10-16 06:35:39 +0800

    Add data

---
+++ utils/data.js
@@ -0,0 +1,37 @@
+const data = {
+  products: [
+    {
+      name: "Free Shirt",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 70,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Fit Shirt",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 50,
+      brand: "Nike",
+      rating: 4.3,
+      numReviews: 10,
+      countInStock: 20,
+      description: "A popular shirt",
+    },
+    {
+      name: "Shirt+",
+      category: "Shirts",
+      image: "/images/shirt1.jpg",
+      price: 10,
+      brand: "Nike",
+      rating: 4.0,
+      numReviews: 9,
+      countInStock: 10,
+      description: "A popular shirt",
+    },
+  ],
+};
