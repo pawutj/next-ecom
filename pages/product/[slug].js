0855f5b710dabd349bcb867f06d9b97b1b7e5bd6
22p26c0377 <pawut.j@gmail.com>
2021-10-20 03:24:19 +0800

    Comaplate Page Product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,7 +5,15 @@ import { data } from "../../utils/data";
 import Layout from "../../components/Layout";
 import NextLink from "next/link";
 import Image from "next/image";
-import { Link, Grid } from "@material-ui/core";
+import {
+  Link,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  Button,
+} from "@material-ui/core";
 import useStyles from "../../utils/styles";
 
 function ProductScreen(props) {
@@ -18,9 +26,11 @@ function ProductScreen(props) {
   }
   return (
     <Layout title={product.name}>
-      <div>
+      <div className={classes.section}>
         <NextLink href="/" passHref>
-          <Link>back to Main</Link>
+          <Typography>
+            <Link>back to Main</Link>
+          </Typography>
         </NextLink>
       </div>
       <Grid container spacing={1}>
@@ -32,6 +42,46 @@ function ProductScreen(props) {
             height={640}
             layout="responsive"
           ></Image>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography>Category: {product.category} </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Brand:{product.brand}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>Rating:{product.rating}</Typography>{" "}
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Price</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>${product.price}</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>In Stock</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography>${product.countInStock}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button fullWidth variant="contained" color="primary">
+                  Add to Cart
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
         </Grid>
       </Grid>
     </Layout>
